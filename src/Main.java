426a40f9c06e73facd279e31c1d06a18d0f67aaf
al-ce <23170004+al-ce@users.noreply.github.com>
2025-07-20 01:47:21 -0500

    refactor: bind key with helper function, check key wait status with HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,25 +8,38 @@ import cell.Cell;
 public class Main {
     private static Plane p;
     private static JFrame frame;
-    private static boolean waitingForSpacebar = true;
-    private static boolean waitingForEscape = true;
     private static Timer loopTimer;
+    private static HashMap<String, Boolean> keyWait;
 
     public static void main(String[] args) {
+        // Attempt to get a custom size from the cli args
         int size;
         try {
             size = Integer.parseInt(args[0]);
-        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+        }
+        // If the arg wasn't a valid integer, exit
+        catch (NumberFormatException e) {
             System.err.println("Must provide an integer argument for grid size");
             return;
+
+        }
+        // If no size was set, default to 40
+        catch (ArrayIndexOutOfBoundsException e) {
+            size = 40;
         }
 
+        // A plane will have all the logic to enact the rules of Life
         p = new Plane(size);
+
+        // frame is the main point of interaction for the app
         frame = new JFrame("Life");
 
+        // Set gaps of 1px. Since the background is set to GRAY, this will give
+        // the illusion of a border around each Cell
         frame.setLayout(new GridLayout(size, size, 1, 1));
         frame.getContentPane().setBackground(Color.GRAY);
 
+        // Add all the cells to the frame
         for (int y = 0; y < p.height; y++) {
             for (int x = 0; x < p.width; x++) {
                 Cell c = p.cells[y][x];
@@ -33,10 +47,12 @@ public class Main {
             }
         }
 
-        setupKeyBinding();
-
+        // Set keybindings to interact with the app
+        keyWait = new HashMap<String, Boolean>();
+        setupKeyBindings();
         setupKeypressLoop(50, p);
 
+        // Finalize the app config
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 1000);
         frame.setResizable(false);
@@ -47,33 +63,33 @@ public class Main {
      * setupKeyBindinding sets up the key bind that progresses a generation on
      * the Plane
      */
-    private static void setupKeyBinding() {
-        // Get the root pane's input and action maps
+    private static void setupKeyBindings() {
+
+        bindKey("escape");
+        bindKey("space");
+
+    }
+
+    /**
+     * bindKey binds a key to an action
+     */
+    private static void bindKey(String key) {
+
+        keyWait.put(key, true);
+
         InputMap inputMap = frame.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
         ActionMap actionMap = frame.getRootPane().getActionMap();
+        inputMap.put(KeyStroke.getKeyStroke(String.format("pressed %s", key.toUpperCase())),
+                String.format("%sPressed", key));
+        actionMap.put(String.format("%sPressed", key), new AbstractAction() {
 
-        // Bind space to action on spacebar press
-        inputMap.put(KeyStroke.getKeyStroke("pressed SPACE"), "spacePressed");
-        actionMap.put("spacePressed", new AbstractAction() {
+            // Reset the waiting state for this key
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (waitingForSpacebar) {
-                    waitingForSpacebar = false;
-                }
+                System.out.printf("key pressed: %s\n", key);
+                keyWait.put(key, false);
             }
         });
-
-        // Bind escape to action on escape press
-        inputMap.put(KeyStroke.getKeyStroke("pressed ESCAPE"), "escapePressed");
-        actionMap.put("escapePressed", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (waitingForEscape) {
-                    waitingForEscape = false;
-                }
-            }
-        });
-
     }
 
     /**
@@ -84,14 +100,14 @@ public class Main {
      */
     private static void setupKeypressLoop(int period, Plane p) {
         loopTimer = new Timer(period, e -> {
-            if (!waitingForSpacebar) {
+            if (!keyWait.get("space")) {
                 p.Evolve();
                 System.out.printf("Progressing to generation %d\n", p.generation);
-                waitingForSpacebar = true;
+                keyWait.put("space", true);
             }
-            if (!waitingForEscape) {
+            if (!keyWait.get("escape")) {
                 p.ClearPlane();
-                waitingForEscape = true;
+                keyWait.put("escape", true);
             }
 
             // Redraw/repaint
